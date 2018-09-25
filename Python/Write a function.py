cd5e57a1240ed8c42498e71422e341dd50bed878
Rahul S <rahulsambyal007@gmail.com>
2018-09-25 20:46:17 +0530

    Add solution of Write a function

---
+++ Python/Write a function.py
@@ -0,0 +1,13 @@
+def is_leap(year):
+    leap = False
+    if year%400 == 0:
+        leap = True
+    elif year%4 == 0:
+        if year%100 == 0:
+            leap = False
+        else:
+            leap = True
+            
+    # Write your logic here
+    
+    return leap
